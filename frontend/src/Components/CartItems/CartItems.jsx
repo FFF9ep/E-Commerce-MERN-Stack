f8d8dc5ebedce4e93af8625aa18899f28f4ee5af
fandi a <fandiiardy@gmail.com>
2024-11-13 21:10:16 +0700

    Update

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -5,7 +5,7 @@ import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
 
-    const {all_products,cartItems,removeFromCart} = useContext(ShopContext);
+    const {all_product,cartItems,removeFromCart} = useContext(ShopContext);
 
     return (
         <div className='cartitems'>
@@ -18,19 +18,20 @@ const CartItems = () => {
                 <p>Remove</p>
             </div>
             <hr />
-            {all_products.map((e)=>{
-                if (cartItems[e.id]>0) {
+            {all_product.map((e)=>{
+                if (cartItems[e.id]>0) 
+                {
                     return <div>
-                    <div className="cartitems-format">
-                        <img src={e.image} alt="" className='carticon-product-icon' />
-                        <p>{e.name}</p>
-                        <p>${e.new_price}</p>
-                        <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                        <p>{e.new_price*cartItems[e.id]}</p>
-                        <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+                                <div className="cartitems-format">
+                                <img src={e.image} alt="" className='carticon-product-icon' />
+                                <p>{e.name}</p>
+                                <p>${e.new_price}</p>
+                                <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                                <p>{e.new_price*cartItems[e.id]}</p>
+                                <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+                            </div>
+                        <hr />
                     </div>
-                    <hr />
-                </div>
                 }
             })}
         </div>
