b02cff035384469986c7887743f1125b7ad2286b
fandi a <fandiiardy@gmail.com>
2024-11-16 21:00:13 +0700

    Update

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,16 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection With MongoDB
-mongoose.connect("mongodb+srv://default:Y2DQ3caNlBOvqIIw@cluster0.1owha.mongodb.net/e-commerce");
+mongoose.connect("mongodb+srv://default:Y2DQ3caNlBOvqIIw@cluster0.1owha.mongodb.net/e-commerce")
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB', err));
+
+// mongoose.connect(process.env.DATABASE, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => console.log('Connected to MongoDB'))
+//     .catch(err => console.error('Error connecting to MongoDB', err));
 
 // API Create
 
@@ -32,7 +41,15 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage:storage})
 
-//Create Upload Endpoint
+//Create Upload Endpoint For Images
+app.use('/images', express.static('upload/images'))
+
+app.post("/upload",upload.single('product'), (req,res) => {
+    res.json({
+        success: 1,
+        Image_url: `http:localhost:${port}/images/${req.file.filename}`
+    })
+})
 
 app.listen(port, (error) => {
     if (!error) {
