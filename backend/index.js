ac31f940b0fd882107dcb883704ecac69d7ca72a
fandi a <fandiiardy@gmail.com>
2024-11-20 13:25:38 +0700

    Add Product Schema

--- backend/index.js
+++ backend/index.js
@@ -60,8 +60,47 @@ app.post("/upload",upload.single('product'), (req,res) => {
         success: 1,
         Image_url: `http:localhost:${port}/images/${req.file.filename}`
     })
-    console.log(req.body)
+    // console.log(req.body)
 })
+
+// Schema Products Creatiaton
+
+const Product = mongoose.model("Product", {
+    id:{
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    new_price: {
+        type: Number,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    }
+})
+
+
 
 app.listen(port, (error) => {
     if (!error) {
