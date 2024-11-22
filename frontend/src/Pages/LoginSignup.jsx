3957878a2964835543db0dbe2b218880d7ae7174
fandi a <fandiiardy@gmail.com>
2024-11-22 20:13:47 +0700

    Update Front

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -4,17 +4,34 @@ import './CSS/LoginSignup.css'
 const LoginSignup = () => {
 
   const [state, setState] = useState("Login");
+  const [formData, setFormData] = useState({
+    username: "",
+    password:"",
+    email:""
+  });
+
+  const changeHandler = (e) => {
+    setFormData({...formData,[e.target.name]: e.target.value})
+  }
+
+  const login = async () => {
+
+  }
+
+  const signup = async () => {
+    
+  }
 
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-field">
-          {state==="Sign Up"?<input type="text" placeholder="Your Name" />:<></>}
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
+          {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder="Your Name" />:<></>}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder="Email Address" />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder="Password" />
         </div>
-        <button>Continue</button>
+        <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
         {state==="Sign Up"?<p className="loginsignup-login">Already have an account? <span onClick={() => {setState("Login")}}>Login Here!</span></p>:<p className="loginsignup-login">Create an account? <span onClick={() => {setState("Sign Up")}}>Sign Up Here!</span></p>}
         <div className="loginsignup-agree">
           <input type="checkbox" name='' id='' />
