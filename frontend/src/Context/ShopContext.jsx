ec9211bf1c45245cd9c2b9d3e622cb6974939961
fandi a <fandiiardy@gmail.com>
2024-11-23 19:42:50 +0700

    All Product Using API

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ShopContext = createContext(null);
 
@@ -13,6 +13,12 @@ const ShopContextProvider = (props) => {
 
     const [all_product, setAll_Product] = useState([]);
     const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    useEffect(() => {
+        fetch('http://localhost:4000/allproducts')
+            .then((response) => response.json())
+            .then((data) => setAll_Product(data))
+    },[]);
     
     const addToCart = (itemId)=> {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
