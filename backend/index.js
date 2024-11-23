d62e564f993a9ad095cbd78c00538dd514b6eff5
fandi a <fandiiardy@gmail.com>
2024-11-23 20:11:18 +0700

    Add Middleware Fetch for User

--- backend/index.js
+++ backend/index.js
@@ -237,9 +237,26 @@ app.get('/popularinwomen', async (req, res) => {
     res.send(popular_in_women);
 })
 
+// Middleware fetch user
+const fetchUser = async (req,res,next) => {
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({errors: "Please autheticate using valid token!"})
+    }
+    else {
+        try {
+            const data = jwt.verify(token, 'secret_ecom');
+            req.user = data.user;
+            next();
+        } catch (error) {
+            res.status(401).send({errors: "Please auth using valid token!"})
+        }
+    }
+}
+
 // Add Product in cartdata
-app.post('/addtocart', async (req, res) => {
-    
+app.post('/addtocart', fetchUser , async (req, res) => {
+    console.log(req.body, req.user);
 })
 
 app.listen(port, (error) => {
